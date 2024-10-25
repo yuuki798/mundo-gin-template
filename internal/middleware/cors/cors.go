a11cbbd19a55f4632bf99b81aba8b666125e64f8
yuuki798 <b19357160114@gmail.com>
2024-10-25 10:32:50 +0800

    feat: cors

---
+++ internal/middleware/cors/cors.go
@@ -0,0 +1,23 @@
+package cors
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// 自定义 CORS 中间件
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent) // 处理 OPTIONS 预检请求
+			return
+		}
+
+		c.Next()
+	}
+}
